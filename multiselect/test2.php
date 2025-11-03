45762bccc1ca1d1685ee6d70ee237089a55a26df
Aniket Singh <anikeths@parasightsolutions.com>
2025-11-03 16:33:37 +0530

    Update test2.php

--- multiselect/test2.php
+++ multiselect/test2.php
@@ -33,6 +33,31 @@
 
 </body>
 
+    <!-- <!-- Include jQuery + Select2 -->
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+<!-- Dropdown -->
+<select id="subject-select" class="form-control" multiple>
+  <option value="math">Math</option>
+  <option value="science">Science</option>
+  <option value="english">English</option>
+</select>
+
+<script>
+$(document).ready(function() {
+  $('#subject-select').select2({
+    tags: true, // allows custom entries
+    placeholder: 'Select or add a subject',
+    allowClear: true,
+    tokenSeparators: [',']
+  });
+});
+</script> -->
+
+
+    
 </html>
 <?php
 print_r($_GET['fruits']);
